c2b53559b5ef953b78ad796a04fdf247bd0e8260
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-01-27 03:46:41 +0100

    Enhance theme manager by adding elevated button theme and refining button theme for improved UI consistency

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,39 +6,58 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager
-          .grey1, // will be used incase of disabled button for example
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager
+        .grey1, // will be used incase of disabled button for example
 
-      //ripple color
+    //ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    colorScheme: ThemeData.light().colorScheme.copyWith(
+          secondary: ColorManager.grey,
+        ),
+
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+
+    // app bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle:
+          getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+    ),
+
+    // button theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
       splashColor: ColorManager.primaryOpacity70,
-      colorScheme: ThemeData.light().colorScheme.copyWith(
-            secondary: ColorManager.grey,
-          ),
-
-      // card view theme
-      cardTheme: CardTheme(
+    ),
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+      textStyle: getRegularStyle(
         color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4,
       ),
+      backgroundColor: ColorManager.primary,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(AppSize.s12),
+        ),
+      ),
+    )),
+    // text theme
 
-      // app bar theme
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.primaryOpacity70,
-        titleTextStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
-      )
-
-      // button theme
-
-      // text theme
-
-      // input decoration theme (text form field)
-      );
+    // input decoration theme (text form field)
+  );
 }
