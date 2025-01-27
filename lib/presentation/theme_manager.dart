a20569caa3dc3586f9604dbada1adea1e009fa6c
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-01-27 03:35:39 +0100

    Enhance theme manager with additional color and card theme configurations; add AppSize constant for consistent spacing

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,27 +1,32 @@
 import 'package:advanced_course_udemy/presentation/color_manager.dart';
+import 'package:advanced_course_udemy/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
+      // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager
+          .grey1, // will be used incase of disabled button for example
+      colorScheme: ThemeData.light().colorScheme.copyWith(
+            secondary: ColorManager.grey,
+          ),
 
-    disabledColor: ColorManager
-        .grey1, // will be used incase of disabled button for example
-    colorScheme: ThemeData.light().colorScheme.copyWith(
-      secondary: ColorManager.grey,
-    ),
+      // card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      )
 
-    // card view theme
+      // app bar theme
 
-    // app bar theme
+      // button theme
 
-    // button theme
+      // text theme
 
-    // text theme
-
-    // input decoration theme (text form field)
-  );
+      // input decoration theme (text form field)
+      );
 }
