2a231d9f49751dcde407e6e1881a6624d7c0272d
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-01-27 03:20:52 +0100

    Add theme manager and new colors for improved UI consistency

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_course_udemy/presentation/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -22,6 +23,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(theme: getApplicationTheme());
   }
 }
---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,27 @@
+import 'package:advanced_course_udemy/presentation/color_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+
+    disabledColor: ColorManager
+        .grey1, // will be used incase of disabled button for example
+    colorScheme: ThemeData.light().colorScheme.copyWith(
+      secondary: ColorManager.grey,
+    ),
+
+    // card view theme
+
+    // app bar theme
+
+    // button theme
+
+    // text theme
+
+    // input decoration theme (text form field)
+  );
+}
