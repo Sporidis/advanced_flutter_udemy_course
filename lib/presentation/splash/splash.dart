1cdf365283aa071efc5c89e407a4c6771626d367
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-01-27 17:36:38 +0100

    Implement onboarding and splash screens with navigation delay

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:advanced_course_udemy/presentation/resources/assets_manager.dart';
 import 'package:advanced_course_udemy/presentation/resources/color_manager.dart';
+import 'package:advanced_course_udemy/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -10,6 +13,28 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.of(context).pushReplacementNamed(Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
