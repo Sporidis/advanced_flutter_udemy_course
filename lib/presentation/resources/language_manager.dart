1a3a76d20256bbdb069c9c60bd64448d356d9f85
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-02-13 18:24:38 +0100

    Add LanguageManager for language type handling and extension methods

---
+++ lib/presentation/resources/language_manager.dart
@@ -0,0 +1,20 @@
+enum LanguageType {
+  english,
+  arabic,
+}
+
+const String arabic = 'ar';
+const String english = 'en';
+
+extension LanguageTypeExtension on LanguageType {
+  String getValue() {
+    switch (this) {
+      case LanguageType.english:
+        return english;
+      case LanguageType.arabic:
+        return arabic;
+      default:
+        return english;
+    }
+  }
+}
