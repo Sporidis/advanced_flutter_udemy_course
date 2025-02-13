925323ffa47a6ae59b8aac5afcdf90fe220e5145
Nikolaos Sporidis <nikolaos.sporidis@student.howest.be>
2025-02-13 18:20:49 +0100

    Add DioFactory class for network requests and integrate pretty_dio_logger for enhanced logging

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -1,3 +1,4 @@
 class Constant {
   static const String baseUrl = 'https://nikolaos0204.wiremockapi.cloud/';
+  static const String token = "get api token here";
 }
---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,42 @@
+import 'package:advanced_course_udemy/app/constant.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const String applicationJson = 'application/json';
+const String contentType = 'Content-Type';
+const String accept = 'Accept';
+const String authorization = 'Authorization';
+const String defaultLanguage = 'language';
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    int timeOut = 60 * 1000; // 1 minute
+
+    Map<String, dynamic> headers = {
+      contentType: applicationJson,
+      accept: applicationJson,
+      authorization: Constant.token,
+      defaultLanguage: "en" // todo get lang from app prefs
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: Constant.baseUrl,
+      connectTimeout: Duration(milliseconds: timeOut),
+      receiveTimeout: Duration(milliseconds: timeOut),
+      headers: headers,
+    );
+
+    if(kReleaseMode){
+      print('Release Mode no logs');
+    } else{
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
+    return dio;
+  }
+}
